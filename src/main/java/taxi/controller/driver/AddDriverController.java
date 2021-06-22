802d3a3dc8b4057551efae53a6dd75217a9a86c2
SerKisJD <sergej.kislyakov@gmail.com>
2021-06-22 14:25:00 +0300

    taxi_service

---
+++ src/main/java/taxi/controller/driver/AddDriverController.java
@@ -0,0 +1,33 @@
+package taxi.controller.driver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+public class AddDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException, ServletException {
+        Driver driver = new Driver(req.getParameter("name"),
+                req.getParameter("licence_number"),
+                req.getParameter("login"),
+                req.getParameter("password"));
+        driverService.create(driver);
+        resp.sendRedirect("/drivers");
+    }
+}
